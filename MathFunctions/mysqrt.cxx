e85fb1290896adfc8b5a36c96d1f242faced92e9
davix <daviseago@gmail.com>
2018-12-13 19:20:31 +0800

    add system introspection: log? exp?

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,4 +1,11 @@
-double mysqrt(double v)
+double mysqrt(double x)
 {
-	return v;
+	double result = 0;
+// if we have both log and exp then use them
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+	result = exp(log(x)*0.5);
+#else // otherwise use an iterative approach
+	result = x;
+#endif
+	return result;
 }
